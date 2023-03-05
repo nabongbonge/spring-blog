da9c3d5c0521e8266dadbc2c1c0f5bb36d6a99b1
nabongbonge <jaeseugi.1927@gmail.com>
2023-03-05 21:41:42 +0900

    회원 가입 기능
    * 회원 가입 화면
    * 회원 컨트롤러
    * 회원 API 컨트롤러
    * 회원 레포지토리
    * 회원 서비스
    * 응답DTO

---
+++ src/main/resources/static/js/user/user.js
@@ -0,0 +1,32 @@
+let index = {
+  init: function () {
+
+    $("#btn-save").on("click",()=>{
+      this.save();
+    });
+
+  },
+  save: function () {
+    let data = {
+      username: $("#username").val(),
+      password: $("#password").val(),
+      email: $("#email").val()
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "/auth/joinProc",
+      data: JSON.stringify(data),
+      contentType: "application/json; charset=tuf-8",
+      dataType: "json"
+    }).done(function (resp) {
+      alert("회원가입이 완료 되었습니다.");
+      console.log(resp);
+      location.href = "/";
+    }).fail(function (error) {
+      alert(JSON.stringify(error));
+    });
+  }
+}
+
+index.init();
