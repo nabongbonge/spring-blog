b8449afe2841abe5fa1105d7fdb2efb5c5ea83ab
nabongbonge <jaeseugi.1927@gmail.com>
2023-03-08 00:20:12 +0900

    카카오 로그인 인증/인가 기능 테스트
    카카오 로그인 인증/인가 기능 테스트

--- src/main/java/com/springblog/controller/UserController.java
+++ src/main/java/com/springblog/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.springblog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -21,5 +30,31 @@ public class UserController {
   @GetMapping("/user/updateForm")
   public String updateForm() {
     return "user/updateForm";
+  }
+
+  @ResponseBody
+  @GetMapping("/auth/kakao/callback")
+  public String kakaoCallback(String code) {
+
+    RestTemplate rt = new RestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("grant_type", "authorization_code");
+    params.add("client_id", "560dcdfcd360106efdc8710475eb840b");
+    params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+    params.add("code", code);
+
+    HttpEntity<MultiValueMap<String, String>> kakaoTokenReq = new HttpEntity<>(params, headers);
+
+    ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com//oauth/token",
+            HttpMethod.POST,
+            kakaoTokenReq,
+            String.class
+    );
+
+    return "카카오 토큰 요청 완료 : 토큰 요청에 대한 응답 : " + response.getBody();
   }
 }
